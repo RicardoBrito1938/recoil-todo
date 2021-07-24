1456e5dcb8835aaeb7314416ccd6826fe8bb32a0
Ricardo <ricardo.jucrist@hotmail.com>
2021-07-24 14:56:12 -0300

    done

---
+++ src/components/Input.tsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react'
+import {useRecoilCallback, useRecoilValue} from 'recoil'
+import styled from 'styled-components'
+import {Container as TaskContainer, TextStyle as TaskTextStyle} from './Task'
+import {tasksState} from './Tasks'
+import {taskState} from './Task'
+
+const InsertInput = styled.input`
+    width: 100%;
+    height: 100%;
+    appearance: none;
+    border: 0;
+    background-color: transparent;
+    outline: none;
+    -webkit-appearance: textfield;
+    ${TaskTextStyle};
+
+    ::-webkit-search-decoration,
+    ::-webkit-search-cancel-button {
+        -webkit-appearance: none;
+    }
+`
+
+export const Input: React.FC = () => {
+    const [label, setLabel] = useState('')
+    const tasks = useRecoilValue(tasksState)
+
+    const insertTask = useRecoilCallback(({set}) => {
+        return (label: string) => {
+            const newTaskId = tasks.length
+            set(tasksState, [...tasks, newTaskId])
+            set(taskState(newTaskId), {
+                label: label,
+                complete: false,
+            })
+        }
+    })
+
+    return (
+        <TaskContainer>
+            <InsertInput
+                placeholder="Insert a new task..."
+                type="search"
+                autoComplete="off"
+                value={label}
+                onChange={({currentTarget}) => {
+                    setLabel(currentTarget.value)
+                }}
+                onKeyUp={({keyCode}) => {
+                    if (keyCode === 13) {
+                        insertTask(label)
+                        setLabel('')
+                    }
+                }}
+            />
+        </TaskContainer>
+    )
+}
---
+++ src/components/Tasks.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { atom, useRecoilValue } from 'recoil'
+import {Task} from './Task'
+
+
+export const tasksState = atom<number[]>({
+    key: 'tasksState',
+    default: []
+})
+
+export const Tasks: React.FC = () => {
+    const tasks = useRecoilValue(tasksState)
+
+    return (
+        <div>
+            {tasks.map(id => <Task key={id} id={id} /> )}   
+        </div>
+    )
+}
